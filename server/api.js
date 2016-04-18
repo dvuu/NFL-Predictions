ed02dbfc486d0050f60116c2ba911125469588b3
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-04-18 13:27:23 -0700

    RESULTS.csv converted to results.json

--- server/api.js
+++ server/api.js
@@ -1,11 +1,21 @@
+var fs = require('fs');
+var GAME_DATA = JSON.parse(fs.readFileSync('../data/games.json'));
+var RESULT_DATA = JSON.parse(fs.readFileSync('../data/results.json'));
+var _ = require('underscore');
+var url = require('url');
+
+var resultsData = RESULT_DATA;
+
 module.exports = function(app) {
 
 	app.get('/api/helloworld', function(req, res) {
 
 		var result = {
-			foo: 'bar'
+			results: [ ]
 		};
-
+		for (var i = 0; i < resultsData.length; ++i ) {
+			result.results.push(resultsData[i]);
+		}
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(result));
     });
