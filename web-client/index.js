aac02a2d50fd92dde7c7907821422692e1db7561
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-04-19 12:47:39 -0700

    dropdown menu and basic graph added

--- web-client/index.js
+++ web-client/index.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-	$.ajax({ url: '/api/helloworld', success: function(result) {
-		alert(result);
-	}});
+	buildGamesList();
 });
+
+function buildGamesList() {
+	$('.gamesDropdown .dropdown-content').empty();
+	$.ajax({ url: '/api/games', success: function(result) {
+		_.each(result, function(game) {
+			if(game.gid >= 2981) {
+				var $listItem = $('<a class="links"></a>');
+				var $div = $('<div><span>Season: ' + game.season + ' - Week: ' + game.week + ' - ' 
+					+ game.home + ' vs. ' + game.visitor + '</span></div>');
+				$listItem.append($div);
+				$('.gamesDropdown .dropdown-content').append($listItem);
+			}
+		});
+	}, error: function() {
+		console.log('error');
+	}});
+}
