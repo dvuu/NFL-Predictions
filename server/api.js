04a02ebb1b1170c43e2d0977ae9b539394de43ec
Anthony Van Pelt <avanpelt@avanpelt-ltm.internal.salesforce.com>
2016-04-19 09:10:25 -0700

    added api routes for games and plays

--- server/api.js
+++ server/api.js
@@ -7,100 +7,44 @@ var url = require('url');
 module.exports = function(app) {
 
 	app.get('/api/games', function(req, res) {
-		var result = [
-			{
-				"gid": 1,
-				"season": 2013,
-				"home": "SEA",
-				"visitors": "GB",
-				"week": 2
-			},
-			{
-				"gid": 2,
-				"season": 2013,
-				"home": "SEA",
-				"visitors": "SF",
-				"week": 3
-			},
-			{
-				"gid": 3,
-				"season": 2013,
-				"home": "SEA",
-				"visitors": "PI",
-				"week": 4
-			},
-			{
-				"gid": 4,
-				"season": 2013,
-				"home": "SEA",
-				"visitors": "NE",
-				"week": 6
-			},
-			{
-				"gid": 5,
-				"season": 2013,
-				"home": "SEA",
-				"visitors": "NY",
-				"week": 7
-			},
-			{
-				"gid": 6,
-				"season": 2013,
-				"home": "SEA",
-				"visitors": "MI",
-				"week": 10
-			}
-		];
+		var result = [ ];
+		for (var i = 0; i < GAME_DATA.length; i++) {
+			var obj = {};
+			obj.gid = GAME_DATA[i].gid;
+			obj.season = GAME_DATA[i].seas;
+			obj.home = GAME_DATA[i].h;
+			obj.visitor = GAME_DATA[i].v;
+			obj.week = GAME_DATA[i].wk;
+			result.push(obj);
+		}
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(result));
     });
 
 	app.get('/api/plays/:gid', function(req, res) {
-
-		var plays = [
-			{
-				"pid": 200,
-				"time": 146,
-				"type": "rush",
-				"prediction": .1285
-			},
-			{
-				"pid": 225,
-				"time": 502,
-				"type": "pass",
-				"prediction": .1120
-			},
-			{
-				"pid": 250,
-				"time": 1023,
-				"type": "pass",
-				"prediction": .13
-			},
-			{
-				"pid": 275,
-				"time": 2253,
-				"type": "rush",
-				"prediction": .1259
-			},
-			{
-				"pid": 300,
-				"time": 2423,
-				"type": "pass",
-				"prediction": .5882
-			},
-			{
-				"pid": 325,
-				"time": 2901,
-				"type": "rush",
-				"prediction": .6002
-			},		
-			{
-				"pid": 350,
-				"time": 3004,
-				"type": "rush",
-				"prediction": .9234
+		var gameId = req.params.gid;
+		var plays = [ ];
+		for (var i = 0; i < RESULT_DATA.length; i++) {
+			if (gameId == RESULT_DATA[i].gid) {
+				var obj = {};
+				obj.playId = RESULT_DATA[i].pid;	
+				obj.time = RESULT_DATA[i].seconds;
+				obj.type = RESULT_DATA[i].type;
+				if (RESULT_DATA[i].v !== RESULT_DATA[i].off) {
+					obj.homePrediction = RESULT_DATA[i].OffWinPred;
+				}
+				else{
+					obj.homePrediction = (1 - RESULT_DATA[i].OffWinPred);
+				}
+				if (RESULT_DATA[i].v == RESULT_DATA[i].off) {
+					obj.visitorPrediction = RESULT_DATA[i].OffWinPred;
+				}
+				else{
+					obj.visitorPrediction = (1 - RESULT_DATA[i].OffWinPred);
+				}
+				plays.push(obj);
 			}
-		];
+		}
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(plays));
     });
