7acd130e32f945ee1bc4b3385db98a10fd24883a
Anthony Van Pelt <avanpelt@avanpelt-ltm.internal.salesforce.com>
2016-04-18 17:07:33 -0700

    hard code api for front-end

--- server/api.js
+++ server/api.js
@@ -4,20 +4,104 @@ var RESULT_DATA = JSON.parse(fs.readFileSync('../data/results.json'));
 var _ = require('underscore');
 var url = require('url');
 
-var resultsData = RESULT_DATA;
-
 module.exports = function(app) {
 
-	app.get('/api/results', function(req, res) {
-
-		var result = {
-			results: [ ]
-		};
-		for (var i = 0; i < resultsData.length; ++i ) {
-			result.results.push(resultsData[i]);
-		}
+	app.get('/api/games', function(req, res) {
+		var result = [
+			{
+				"gid": 1,
+				"season": 2013,
+				"home": "SEA",
+				"visitors": "GB",
+				"week": 2
+			},
+			{
+				"gid": 2,
+				"season": 2013,
+				"home": "SEA",
+				"visitors": "SF",
+				"week": 3
+			},
+			{
+				"gid": 3,
+				"season": 2013,
+				"home": "SEA",
+				"visitors": "PI",
+				"week": 4
+			},
+			{
+				"gid": 4,
+				"season": 2013,
+				"home": "SEA",
+				"visitors": "NE",
+				"week": 6
+			},
+			{
+				"gid": 5,
+				"season": 2013,
+				"home": "SEA",
+				"visitors": "NY",
+				"week": 7
+			},
+			{
+				"gid": 6,
+				"season": 2013,
+				"home": "SEA",
+				"visitors": "MI",
+				"week": 10
+			}
+		];
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(result));
     });
 
+	app.get('/api/plays/:gid', function(req, res) {
+
+		var plays = [
+			{
+				"pid": 200,
+				"time": 146,
+				"type": "rush",
+				"prediction": .1285
+			},
+			{
+				"pid": 225,
+				"time": 502,
+				"type": "pass",
+				"prediction": .1120
+			},
+			{
+				"pid": 250,
+				"time": 1023,
+				"type": "pass",
+				"prediction": .13
+			},
+			{
+				"pid": 275,
+				"time": 2253,
+				"type": "rush",
+				"prediction": .1259
+			},
+			{
+				"pid": 300,
+				"time": 2423,
+				"type": "pass",
+				"prediction": .5882
+			},
+			{
+				"pid": 325,
+				"time": 2901,
+				"type": "rush",
+				"prediction": .6002
+			},		
+			{
+				"pid": 350,
+				"time": 3004,
+				"type": "rush",
+				"prediction": .9234
+			}
+		];
+		res.writeHead(200,{'Content-Type': 'application/json'});
+        res.end(JSON.stringify(plays));
+    });
 };
