25d8929645d083ea2935200a9a01d36967f3aefc
Anthony Van Pelt <avanpelt@avanpelt-ltm.internal.salesforce.com>
2016-04-19 14:15:26 -0700

    refactored api/games, added api for games by season

--- server/api.js
+++ server/api.js
@@ -4,21 +4,51 @@ var RESULT_DATA = JSON.parse(fs.readFileSync('../data/results.json'));
 var _ = require('underscore');
 var url = require('url');
 
+function games() {
+	var result = [ ];
+	for (var i = 0; i < GAME_DATA.length; i++) {
+		var obj = {
+			gameId: GAME_DATA[i].gid,
+			season: GAME_DATA[i].seas,
+			home: GAME_DATA[i].h,
+			visitor: GAME_DATA[i].v,
+			week: GAME_DATA[i].wk
+		};
+		result.push(obj);
+	}
+	return result;
+};
+
 module.exports = function(app) {
 
+	// Returns list of all games
 	app.get('/api/games', function(req, res) {
-		var result = [ ];
-		for (var i = 0; i < GAME_DATA.length; i++) {
-			var obj = {};
-			obj.gid = GAME_DATA[i].gid;
-			obj.season = GAME_DATA[i].seas;
-			obj.home = GAME_DATA[i].h;
-			obj.visitor = GAME_DATA[i].v;
-			obj.week = GAME_DATA[i].wk;
-			result.push(obj);
-		}
+		console.log("Client requested list of all games...");
+		var result = games();
 		res.writeHead(200,{'Content-Type': 'application/json'});
-        res.end(JSON.stringify(result));
+	    res.end(JSON.stringify(result));
+	});
+	// Return list of games from a season
+	app.get('/api/gamesBySeason/:seas', function(req, res) {
+		var season = req.params.seas;
+		console.log("Client requested list of all games from season: " + season + "...");
+		var allGames = games();
+		var result = [ ];
+		for (var i = 0; i < allGames.length; i++) {
+		 	if(allGames[i].season == season){
+		 		result.push(allGames[i]);
+		 	}
+	 	}
+		res.writeHead(200,{'Content-Type': 'application/json'});
+    	res.end(JSON.stringify(result));
+    });
+
+	app.get('/api/gamesByWeek/:seas/:week', function(req, res) {
+		var season = req.params.seas;
+		var week = req.params.week;
+
+		res.writeHead(200,{'Content-Type': 'application/json'});
+        res.end(JSON.stringify(plays));
     });
 
 	app.get('/api/plays/:gid', function(req, res) {
@@ -46,4 +76,5 @@ module.exports = function(app) {
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(plays));
     });
+
 };
