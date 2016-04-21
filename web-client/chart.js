d76a7fdf130d633590a171129e2cc0ccd4ef926e
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-04-21 01:38:49 -0700

    added chart and functionality to populate chart based on game

--- web-client/chart.js
+++ web-client/chart.js
@@ -1,6 +1,6 @@
 // look at game 3987
 
-$.ajax({ url: '/api/plays/3987', success: function(result) {
+function buildChartFromData(result, title) {
     var playSeries = {
         x: [ ],
         y: [ ],
@@ -22,48 +22,23 @@ $.ajax({ url: '/api/plays/3987', success: function(result) {
     })
 
     var layout = {
+        title: title,
         xaxis: {
+            title: 'Game time left',
             autorange: 'reversed'
         },
         yaxis: {
+            title: 'Prediction',
             range: [0, 1]
         }
-    }
+    };
 
     var data = [playSeries];
 
     Plotly.newPlot('chart', data, layout);
-}});
+}
 
 
 
 
-    // var selectorOptions = {
-    //     buttons: [{
-    //         step: 'month',
-    //         stepmode: 'backward',
-    //         count: 1,
-    //         label: '1m'
-    //     },
-    //     {
-    //         step: 'month',
-    //         stepmode: 'backward',
-    //         count: 6,
-    //         label: '6m'
-    //     },
-    //     {
-    //         step: 'year',
-    //         stepmode: 'todate',
-    //         count: 1,
-    //         label: 'YTD'
-    //     },
-    //     {
-    //         step: 'year',
-    //         stepmode: 'backward',
-    //         count: 1,
-    //         label: '1y'
-    //     },
-    //     {
-    //         step: 'all',
-    //     }],
-    // };
+    
--- web-client/index.js
+++ web-client/index.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
-	buildGamesList();
+	buildGamesFilter();
+	buildChartFromData();
 });
 
-function buildGamesList() {
+function fetchAndDisplayPlays(url, title) {
+	$.ajax({ url: url, success: function(result) {
+		buildChartFromData(result, title);
+	}});
+}
+
+function buildGamesFilter() {
 	$('.gamesDropdown .dropdown-content').empty();
 	$.ajax({ url: '/api/games', success: function(result) {
 		_.each(result, function(game) {
-			if(game.gameId >= 2981) {
-				var $listItem = $('<a class="links"></a>');
-				var $div = $('<div><span>Season: ' + game.season + ' - Week: ' + game.week + ' - ' 
-					+ game.home + ' vs. ' + game.visitor + '</span></div>');
-				$listItem.append($div);
-				$('.gamesDropdown .dropdown-content').append($listItem);
-			}
+			var $listItem = $('<a class="links"></a>');
+			var $div = $('<div><span>Season: ' + game.season + ' - Week: ' + game.week 
+				+ ' - ' + game.home + ' vs. ' + game.visitor + '</span></div>');
+			$listItem.append($div);
+			$listItem.click(function(e) {
+				var playUrl = '/api/plays/' + game.gameId;
+				fetchAndDisplayPlays(playUrl, 'Season: ' + game.season + '- Week: ' + 
+					game.week + ' - H: ' + game.home + ' vs. V: ' + game.visitor);
+			});
+			$('.gamesDropdown .dropdown-content').append($listItem);
 		});
 	}, error: function() {
 		console.log('error');
