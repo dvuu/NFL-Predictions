65528333138635931e6d8536967948478a2a6403
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-04-27 11:44:51 -0700

    adjusted y plot values + y axis

--- web-client/chart.js
+++ web-client/chart.js
@@ -10,24 +10,31 @@ function buildChartFromData(plays, title, lastPlay, homeTeam) {
     };
     _.each(plays, function(play) {
         var string = 'Play: ' + play.type;
+        var homeWp = (play.homeWp * 100);
         playSeries.text.push(string);
         playSeries.x.push(play.time);
-        playSeries.y.push(play.homeWp);
+        playSeries.y.push(homeWp.toFixed(2));
     });
     var layout = {
         title: title,
         xaxis: {
-            title: 'Game time left',
+            title: 'Game time remaining (seconds)',
             range: [3700, (lastPlay + -100)], // add padding so the labels aren't slammed against the axes
             dtick: 900,
             zeroline: false
         },
         yaxis: {
-            title: homeTeam + ' Win Probability',
-            range: [0, 1]
+            title: homeTeam + ' Win Probability (%)',
+            range: [0, 100]
         },
         showLegend: true,
         range: [-0.1, 1], // add padding so the labels aren't slammed against the axes
+
+         font: {
+            family: 'Helvetica',
+            size: 18,
+            color: '#7f7f7f'
+        }
     }
     var data = [playSeries];
     Plotly.newPlot('chart', data, layout, {displayModeBar: false});
