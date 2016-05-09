1a225f0cb17a18cce372987cfdbbb814cd01aeab
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-05-09 00:06:57 -0700

    added more info to strings

--- web-client/chart.js
+++ web-client/chart.js
@@ -51,7 +51,8 @@ function buildChartFromData(playsResult, topTenResult, game) {
         title: chartTitle(game),
         xaxis: {
             title: 'Game time remaining (seconds)',
-            range: [3700, (lastPlay + -100)], // add padding so the labels aren't slammed against the axes
+            // add padding so the labels aren't slammed against the axes
+            range: [3700, (lastPlay + -100)],
             dtick: 900,
             zeroline: false
         },
@@ -60,7 +61,8 @@ function buildChartFromData(playsResult, topTenResult, game) {
             range: [0, 100]
         },
         showLegend: true,
-        range: [-0.1, 1], // add padding so the labels aren't slammed against the axes
+        // add padding so the labels aren't slammed against the axes
+        range: [-0.1, 1],
 
         font: {
             family: 'Helvetica',
@@ -86,38 +88,97 @@ function buildChartFromData(playsResult, topTenResult, game) {
     Plotly.newPlot('chart', [playSeries], layout, {displayModeBar: false});
 
     function playInfoString(playsResult) {
-        return ('<p>' + playsResult.visitor + ': ' + playsResult.ptsVisitor + ' @ ' 
-            + playsResult.home + ': ' + playsResult.ptsHome + '</p>');
+        var homeWp = (playsResult.homeWp * 100).toFixed(2);
+        var visitorWp = (playsResult.visitorWp * 100).toFixed(2);
+        if (playsResult.homeWp < 50) {
+            if (playsResult.seconds < 10) {
+                return ('<p>Q' + playsResult.quarter + ', ' + playsResult.minute + ':0' 
+                    + playsResult.seconds 
+                    + '<br>' + playsResult.visitor + ': ' + playsResult.ptsVisitor 
+                    + '<span class="posWp"> (' + visitorWp + '%)</span>' 
+                    + '<br>' + playsResult.home + ': ' + playsResult.ptsHome 
+                    + '<span class="negWp"> (' + homeWp + '%)</span>'
+                    + '<br>Offense: ' + playsResult.offense 
+                    + '<br>Play: ' + playsResult.type 
+                    + '<br>Yard: ' + playsResult.offYardline + '</p>');
+            }
+            else {
+                return ('<p>Q' + playsResult.quarter + ', ' + playsResult.minute + ':' 
+                    + playsResult.seconds 
+                    + '<br>' + playsResult.visitor + ': ' + playsResult.ptsVisitor 
+                    + '<span class="posWp"> (' + visitorWp + '%)</span>' 
+                    + '<br>' + playsResult.home + ': ' + playsResult.ptsHome 
+                    + '<span class="negWp"> (' + homeWp + '%)</span>'
+                    + '<br>Offense: ' + playsResult.offense 
+                    + '<br>Play: ' + playsResult.type 
+                    + '<br>Yard: ' + playsResult.offYardline + '</p>');
+            }
+        }
+        else {
+            if (playsResult.seconds < 10) {
+                return ('<p>Q' + playsResult.quarter + ', ' + playsResult.minute + ':0' 
+                    + playsResult.visitor + ': ' + playsResult.ptsVisitor 
+                    + '<span class="negWp"> (' + visitorWp + '%)</span>' 
+                    + '<br>' + playsResult.home + ': ' + playsResult.ptsHome 
+                    + '<span class="posWp"> (' + homeWp + '%)</span>' 
+                    + playsResult.seconds + '<br>Offense: ' + playsResult.offense 
+                    + '<br>Play: ' + playsResult.type 
+                    + '<br>Yard: ' + playsResult.offYardline + '</p>');
+            }
+            else {
+                return ('<p>Q' + playsResult.quarter + ', ' + playsResult.minute + ':' 
+                    + playsResult.visitor + ': ' + playsResult.ptsVisitor 
+                    + '<span class="negWp"> (' + visitorWp + '%)</span>' 
+                    + '<br>' + playsResult.home + ': ' + playsResult.ptsHome 
+                    + '<span class="posWp"> (' + homeWp + '%)</span>' 
+                    + playsResult.seconds + '<br>Offense: ' + playsResult.offense 
+                    + '<br>Play: ' + playsResult.type 
+                    + '<br>Yard: ' + playsResult.offYardline + '</p>');
+            }
+        }
     }
 
     function swingString(playsResult) {
         var homeWpDiff = (playsResult.homeWpDiff * 100).toFixed(2);
+        var visitorWpDiff = (playsResult.visitorWpDiff * 100).toFixed(2);
         if (playsResult.home == playsResult.offense) {
             if (homeWpDiff > 0) {
                 return ('<p><span class="arrow">→</span>' 
-                    + '<br><span class="posWp swing">+' + homeWpDiff + '%</span>' 
-                    + '<br>Offense: ' + playsResult.offense + '<br>Score: +' 
-                    + playsResult.ptsHomeGain + '</p>');
+                    + '<br><span class="swing">' + playsResult.visitor 
+                    + '<span class="negWp"> (' + visitorWpDiff + '%)</span>' 
+                    + '<br>' + playsResult.home 
+                    + '<span class="posWp"> (+' + homeWpDiff + '%)</span></span>' 
+                    + '<br>Score: +' + playsResult.ptsHomeGain 
+                    + '<br>Yard: +' + playsResult.homeYdsGained + '</p>');
             }
             else {
                 return ('<p><span class="arrow">→</span>' 
-                    + '<br><span class="negWp swing">' + homeWpDiff + '%</span>' 
-                    + '<br>Offense: ' + playsResult.offense + '<br>Score: +' 
-                    + playsResult.ptsHomeGain + '</p>');
+                    + '<br><span class="swing">' + playsResult.visitor 
+                    + '<span class="posWp"> (+' + visitorWpDiff + '%)</span>' 
+                    + '<br>' + playsResult.home 
+                    + '<span class="negWp"> (' + homeWpDiff + '%)</span></span>' 
+                    + '<br>Score: +' + playsResult.ptsHomeGain 
+                    + '<br>Yard: +' + playsResult.homeYdsGained + '</p>');
             }
         }
         else {
             if (homeWpDiff > 0) {
                 return ('<p><span class="arrow">→</span>' 
-                    + '<br><span class="posWp swing">+' + homeWpDiff + '%</span>' 
-                    + '<br>Offense: ' + playsResult.offense + '<br>Score: +' 
-                    + playsResult.ptsVisitorGain + '</p>');
+                    + '<br><span class="swing">' + playsResult.visitor 
+                    + '<span class="negWp"> (' + visitorWpDiff + '%)</span>' 
+                    + '<br>' + playsResult.home 
+                    + '<span class="posWp"> (+' + homeWpDiff + '%)</span></span>' 
+                    + '<br>Score: +' + playsResult.ptsVisitorGain 
+                    + '<br>Yard: +' + playsResult.visitorYdsGained + '</p>');
             }
             else {
                 return ('<p><span class="arrow">→</span>' 
-                    + '<br><span class="negWp swing">' + homeWpDiff + '%</span>' 
-                    + '<br>Offense: ' + playsResult.offense + '<br>Score: +' 
-                    + playsResult.ptsVisitorGain + '</p>');
+                    + '<br><span class="swing">' + playsResult.visitor 
+                    + '<span class="posWp"> (+' + visitorWpDiff + '%)</span>' 
+                    + '<br>' + playsResult.home 
+                    + '<span class="negWp"> (' + homeWpDiff + '%)</span></span>' 
+                    + '<br>Score: +' + playsResult.ptsVisitorGain 
+                    + '<br>Yard: +' + playsResult.visitorYdsGained + '</p>');
             }
         }
     }
