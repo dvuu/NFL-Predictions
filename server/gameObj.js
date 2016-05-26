79e189a85d034a87611f2ee04d287fb3c3e391d9
Anthony Van Pelt <avanpelt@avanpelt-ltm.internal.salesforce.com>
2016-05-26 15:02:41 -0700

    quick changes for refactoring for OO programming

--- server/gameObj.js
+++ server/gameObj.js
@@ -1,10 +1,10 @@
 module.exports.Game = function (game) {
-	this.season = season;
-	this.week = week;
-	this.gameId = gameId;
-	this.home = home;
-	this.visitor = visitor;
-	this.plays = playIds;
-	this.ptsHome = ptsHome;
-	this.ptsVisitor = ptsVisitor;
+	this.season = game.season;
+	this.week = game.week;
+	this.gameId = game.gameId;
+	this.home = game.home;
+	this.visitor = game.visitor;
+	this.plays = game.playIds;
+	this.ptsHome = game.ptsHome;
+	this.ptsVisitor = game.ptsVisitor;
 }
