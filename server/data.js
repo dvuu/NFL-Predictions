7a58c8a759f27b1816c06b3ce9c726134a5f53e0
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-06-03 15:39:13 -0700

    refactored creatPlaysAndGame into data.js

--- server/data.js
+++ server/data.js
@@ -1,36 +1,58 @@
 var _ = require('underscore');
 var csv = require('../utilities/csv.js');
 
+// Game object contains information about the game
+//    e.g. home, visitor, id
+// ALSO contains a collection (array probably) of plays (Play objects)
+var GameObj = require('./gameObj.js');
+var Game = GameObj.Game;
+// Play object contains information about the play -
+//    e.g. id, time, ptso, etc...
+var PlayObj = require('./playObj.js');
+var Play = PlayObj.Play;
+// Could optionally create a Season object
+
 module.exports = {
     initialize: function(callback) {
         var self = this;
-	    csv.readCsv('../data/PLAY_WITH_RESULTS.csv', function(err, resultData) {
-            self.PLAYS = resultData;
-            csv.readCsv('../data/GAME.csv', function(err, gameData) {
-                self.GAMES = gameDataSubset(gameData);
+        csv.readCsv('../data/GAME.csv', function(err, gamesDataRaw) {
+            self.GAMES = createGames(gamesDataRaw);
+    	    csv.readCsv('../data/PLAY_WITH_RESULTS.csv', function(err, playsDataRaw) {
+                self.PLAYS = createPlays(playsDataRaw, gamesDataRaw, self.GAMES);
                 callback && callback();
             });
         });
     },
-
-    PLAYS: null,
     GAMES: null,
+    PLAYS: null,
 }
 
-// grabs basic information from all games
-function gameDataSubset(gameData) {
-    var result = [ ];
-    for (var i = 0; i < gameData.length; ++i) {
-        var obj = {
-            gameId: gameData[i].gid,
-            season: gameData[i].seas,
-            week: gameData[i].wk,
-            home: gameData[i].h,
-            visitor: gameData[i].v,
-            ptsHome: gameData[i].ptsh,
-            ptsVisitor: gameData[i].ptsv
-        };
-        result.push(obj);
+function createGames(gamesDataRaw) {
+    var games = { };
+    for (var i = 2980; i < gamesDataRaw.length; i++) {
+        var gameObj = new Game(gamesDataRaw[i]);
+        games[gameObj.gameId] = gameObj;
     }
-    return result;
-};
+    return games;
+}
+
+function createPlays(playsDataRaw, gamesDataRaw, games) {
+    var plays = [ ];
+    var idx = 0;
+    for (var i = 0; i < playsDataRaw.length; i++) {
+        var isLastPlay = false;
+        var currentPlay = playsDataRaw[i];
+        var nextPlay = (i < playsDataRaw.length - 1) ? (playsDataRaw[i + 1]) : (null);
+        var gameData = gamesDataRaw[currentPlay.gid - 1];
+        if(!nextPlay || currentPlay.gid !== nextPlay.gid) {
+            isLastPlay = true;
+        }
+        var playObj = new Play(currentPlay, nextPlay, idx, gameData, isLastPlay);
+        var game = games[playObj.gameId];
+        game.addPlay(playObj);
+        plays.push(playObj);
+
+        idx = isLastPlay ? 0 : idx + 1;
+    }
+    return plays;
+}
