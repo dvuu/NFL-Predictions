2044682ef0e76969d6a9c17a6f8d9a18f820f3ee
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-06-07 14:06:23 -0700

    simplify api

--- server/api.js
+++ server/api.js
@@ -19,8 +19,10 @@ module.exports = function(app) {
 		var season = req.params.season;
 		console.log("Client requested list of all games from season: " + season + "...");
 		var result = [ ];
-		_.each(data.GAMES, function (Game) {
-			season == Game.season ? result.push(Game): null;
+		_.each(data.GAMES, function (game) {
+			if (season == game.season) {
+				result.push(game);
+			}
     	});
     	res.writeHead(200,{'Content-Type': 'application/json'});
 	    res.end(JSON.stringify(result));
@@ -32,8 +34,10 @@ module.exports = function(app) {
 		var week = req.params.week;
 		console.log("Client requested list of all games from season: " + season + ": Week " + week + "...");
 		var result = [ ];
-		_.each(data.GAMES, function (Game) {
-			season == Game.season && week == Game.week ? result.push(Game) : null; 
+		_.each(data.GAMES, function (game) {
+			if (season == game.season && week == game.week) {
+				result.push(game);
+			}
 		});
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(result));
@@ -42,11 +46,9 @@ module.exports = function(app) {
 	// returns all plays
 	app.get('/api/plays/:gameId', function(req, res) {
 		var gameId = req.params.gameId;
-		var plays = undefined;
 		console.log("Client requested plays from game " + gameId + "...");
-		_.each(data.GAMES, function (Game) {
-			gameId == Game.gameId ? plays = Game.plays : null;
-		});
+		var game = data.GAMES[gameId];
+		var plays = game.plays;
 		res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(plays));
     });
@@ -54,35 +56,38 @@ module.exports = function(app) {
     // returns top 10 plays from a game
     app.get('/api/topTen/:gameId', function(req, res) {
     	var gameId = req.params.gameId;
-    	var plays = undefined;
     	console.log("Client requested top ten plays for game " + gameId + "...");
-    	_.each(data.GAMES, function (Game) {
-			gameId == Game.gameId ? plays = Game.plays : null;
-		});
+    	var game = data.GAMES[gameId];
+    	var topPlays = game.findBiggestPlays(10);
 		res.writeHead(200,{'Content-Type': 'application/json'});
-        res.end(JSON.stringify(findBiggestPlays(plays, 10)));
+        res.end(JSON.stringify(topPlays));
     });
 
     //returns top 10 plays from all games combined 
     app.get('/api/topTen', function(req, res) {
     	console.log("Client is requested Top Ten plays from all all games combined...")
-    	var result = [ ];
-    	for (var i = 0; i < data.PLAYS.length - 1; ++i) {
-    		if (data.PLAYS[i].gid !== data.PLAYS[i + 1].gid) {
-    			continue;
-    		}
-    		var curentWP = (1 - data.PLAYS[i].VisitorWP);
-			var futureWP = (1 - data.PLAYS[i + 1].VisitorWP);
-			var obj = {
-    			gameId: data.PLAYS[i].gid,
-				playId: data.PLAYS[i].pid,
-				type: data.PLAYS[i].type,
-				time: fixSecondsForOvertime(data.PLAYS[i].InOT, data.PLAYS[i].Seconds),
-				homeWpDiff: futureWP - curentWP
-    		};
-	    	result.push(obj);	
-    	}
-	var allTopTen = findBiggestPlays(result, 100)
+	// TODO:
+	// 1. get top n plays from each game, store in an array
+	// 2. sort that array
+	// 3. return first n plays from that array
+	    	
+  //   	var result = [ ];
+  //   	for (var i = 0; i < data.PLAYS.length - 1; ++i) {
+  //   		if (data.PLAYS[i].gid !== data.PLAYS[i + 1].gid) {
+  //   			continue;
+  //   		}
+  //   		var curentWP = (1 - data.PLAYS[i].VisitorWP);
+		// 	var futureWP = (1 - data.PLAYS[i + 1].VisitorWP);
+		// 	var obj = {
+  //   			gameId: data.PLAYS[i].gid,
+		// 		playId: data.PLAYS[i].pid,
+		// 		type: data.PLAYS[i].type,
+		// 		time: fixSecondsForOvertime(data.PLAYS[i].InOT, data.PLAYS[i].Seconds),
+		// 		homeWpDiff: futureWP - curentWP
+  //   		};
+	 //    	result.push(obj);	
+  //   	}
+		// var allTopTen = findBiggestPlays(result, 100)
     	res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(JSON.stringify(allTopTen));
     });	
