2bf72495857760ce26e8e3e5c802140e6830d8f9
Dylan Vu <dylan.vu@dylanvu-ltm.internal.salesforce.com>
2016-06-20 15:13:49 -0700

    added methods for Chart object

--- src/client/js/chartObj.js
+++ src/client/js/chartObj.js
@@ -1,11 +1,7 @@
 (function () {
-    if (!window.NFL) {
-        window.NFL = { };
-    }
-    var NFL = window.NFL;
-    NFL.Chart = function (playsResult, topTenResult, game) {
-        var lastPlay = playsResult[playsResult.length-1].time;
-
+    var NFL = window.NFL = (window.NFL || { });
+    NFL.Chart = function (plays, topTenPlays, game) {
+        var lastPlay = plays[plays.length-1].time;
         this.playSeries = {
             y : [ ],
             x : [ ],
@@ -55,8 +51,99 @@
                 }
             ]
         };
-    }
+        this.setPlays(plays);
+    };
+
     NFL.Chart.prototype.render = function() {
+        var self = this;
         Plotly.newPlot('chart', [this.playSeries], this.layout, {displayModeBar: false});
+        $('#chart')[0].on('plotly_hover', function (data) {
+            self.onHover(data, self.plays);
+        });
+        $('#chart')[0].on('plotly_unhover', function(data) {
+            NFL.PlayInfo.clearPlayInfo();
+        });
+    };
+
+    // Sets plays for instance of chart
+    NFL.Chart.prototype.setPlays = function(plays) {
+        this.plays = plays;
+        this.updateSeries();
+    };
+
+    NFL.Chart.prototype.setLegend = function(play) {
+        if (play.home == play.offense) {
+            return '#ff7400';
+        }
+        else{
+            return '#0021c5';
+        }
+    };
+
+    // Populates series with new data
+    NFL.Chart.prototype.updateSeries = function() {
+        this.playSeries.x = [ ];
+        this.playSeries.y = [ ];
+        this.playSeries.text = [ ];
+        this.playSeries.marker.color = [ ];
+        for (var i = 0; i < this.plays.length; i++) {
+            var play = this.plays[i];
+            var homeWp = (play.homeWp * 100).toFixed(2);
+            var string = play.home + ': ' + homeWp + '%';
+            this.playSeries.text.push(string);
+            this.playSeries.x.push(play.time);
+            this.playSeries.y.push(homeWp);
+            this.playSeries.marker.color.push(this.setLegend(play));
+        }
+    };
+
+    // Hover event handler
+    NFL.Chart.prototype.onHover = function(data, plays) {
+        // Check for weird, unexpected input, just cause
+        if (data.points.length <= 0)
+            return;
+        if (data.points.length > 1) {
+            console.error("Only expected one point for hover");
+            return;
+        }
+
+        // Identify hovered-over play and previous play
+        var point = data.points[0];
+        var index = point.pointNumber;
+        var playOne = (index > 0) ? plays[index - 1] : null;
+        var playTwo = plays[index];
+        
+        // Make both play and previous play show hover tooltip
+        if (index > 0) {
+            Plotly.Fx.hover('chart',[
+                {curveNumber:0, pointNumber: index},
+                {curveNumber:0, pointNumber: index - 1}
+            ]);
+        }
+
+        // Update the play-info section to show hovered plays
+        NFL.PlayInfo.showPlayInfo(playOne, playTwo);
     };
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
