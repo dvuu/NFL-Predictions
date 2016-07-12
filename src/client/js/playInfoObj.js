29a7947b0edd5072396c9356ad79a5257968fd68
Anthony Van Pelt <avanpelt@avanpelt-ltm.internal.salesforce.com>
2016-07-12 00:38:55 -0700

    created scoring play Score Type from two functions called pointLead and pointsCall

--- src/client/js/playInfoObj.js
+++ src/client/js/playInfoObj.js
@@ -3,13 +3,16 @@ var NFL = window.NFL = (window.NFL || { });
 NFL.PlayInfo = {
 	showPlayInfo: function (playOne, playTwo) {
 		if (playOne) {
+            //current play
 	    	$('.playOne').html(playInfoString(playOne));
 	    }
 	    if (playTwo) {
+            //next play
 	    	$('.playTwo').html(playInfoString(playTwo));
 	    }
 	    if (playOne && playTwo) {
-	    	$('.playSwing').html(swingString(playOne));
+            //current play and next play
+	    	$('.playSwing').html(swingString(playOne, playTwo));
 	    }
     },
     clearPlayInfo: function () {
@@ -39,20 +42,55 @@ function playInfoStringHelper (play, class1, class2) {
         + '<br>Ball On: ' + ballOn(play.offYardline) + '</p>');
 };
 
+function pointLead(currentPlay, nextPlay) {
+    var team1 = currentPlay.offense;
+    var team2 = currentPlay.defense;
+    var team1Pts = currentPlay.ptsOffense;
+    var team2Pts = currentPlay.ptsDefense;
+    var nextTeam1Pts = nextPlay.offense == team1 ? nextPlay.ptsOffense : nextPlay.ptsDefense;
+    var nextTeam2Pts = nextPlay.defense == team2 ? nextPlay.ptsDefense : nextPlay.ptsOfense;
+    if (team1Pts != nextTeam1Pts) {
+        return Math.abs(nextTeam1Pts - team1Pts);
+    }
+    else if (team2Pts != nextTeam2Pts) {
+        return Math.abs(nextTeam2Pts - team2Pts);
+    }
+}
+
+function pointsCall(currentPlay, nextPlay) {
+    switch(pointLead(currentPlay, nextPlay)){
+        case 2:
+            // if the case is 2 pts it's a safety and give 2 pts to def
+            return 'Safety'
+        case 3:
+            // if the case is 3 pts it's a regular field goal and give 3 pts to off
+            return 'Regular Feild Goal'
+        case 6:
+            // if the case is 6 pts it's just a touchdown and give 6 pts to off
+            return 'Only a Touchdown'
+        case 7:
+            // if the case is 7 pts it's a touchdown with a field goal and give 7 pts to off
+            return 'Touchdown & Feild Goal'
+        case 8:
+            // if the case is 8 pts it's just a touchdown with a convertion and give 8 pts to def
+            return 'Touchdown & Convertion'
+    }
+}
+
 function ballOn (yardLine) {
 	return yardLine < 50 ? ('OWN ' + yardLine) : (yardLine > 50 ? ('OPP ' + (100 - yardLine)) : '50');
 };
 
-function swingString (playOne) {
+function swingString (playOne, playTwo) {
     if (playOne.homeWpDiff > 0) {
-        return (swingStringHelper(playOne, 'posWp', playOne.pointsScored, playOne.ydsGained));
+        return (swingStringHelper(playOne, 'posWp', playOne.pointsScored, playOne.ydsGained, playTwo));
     }
     else {
-        return (swingStringHelper(playOne, 'negWp', playOne.pointsScored, playOne.ydsGained));
+        return (swingStringHelper(playOne, 'negWp', playOne.pointsScored, playOne.ydsGained, playTwo));
     }
 };
 
-function swingStringHelper(play, class1, ptsGain, ydsGain) {
+function swingStringHelper(play, class1, ptsGain, ydsGain, playTwo) {
     var homeWpDiff = (play.homeWpDiff * 100).toFixed(2);
     if (class1 == 'posWp'){
         homeWpDiff = '+' + homeWpDiff;
@@ -62,5 +100,10 @@ function swingStringHelper(play, class1, ptsGain, ydsGain) {
         + ' <span class="' + class1 + '">' + homeWpDiff + '%</span></span>'
         + '<br>Play: ' + play.type 
         + (ptsGain ? '<br>Points Scored: ' + ptsGain : '')
-        + (!ptsGain ? '<br>Gained: ' + ydsGain + ' yds' : '') + '</p>');
+        + (!ptsGain ? '<br>Gained: ' + ydsGain + ' yds' : '')
+        + (pointsCall(play, playTwo) ? '<br>Score Type: ' + pointsCall(play, playTwo) +'</p>' : ''));
 };
+
+
+
+
