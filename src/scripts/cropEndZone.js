22ada7f966734f3f85a092699d2933567cebe2cc
Anthony Van Pelt <avanpelt@avanpelt-ltm.internal.salesforce.com>
2016-07-26 10:44:31 -0700

    added to the tools scripts for the fields and end zones for the rest of the teams

--- src/scripts/cropEndZone.js
+++ src/scripts/cropEndZone.js
@@ -21,10 +21,45 @@ var teams = [
 	'NO',
 	'MIN',
 	'NYG',
-	'NYJ'
+	'NYJ',
+	'SF',
+	'TB',
+	'TEN',
+	'DET',
+	'HOU',
+	'JAC',
+	'OAK',
+	'LA',
+	'WAS',
+	'CLE',
+	'CIN'
 ]
 
-for (var i = teams.length - 1; i >= 0; i--) {
-	shell.exec('convert fields/' + teams[i] + '.png -crop 78x428+29+29 endzones/' + teams[i] + '.png');
-}
 
+
+for (var i = teams.length - 1; i >= 0; i--) {
+	var teamFeildTypeOne = teams[i] == 'HOU' || teams[i] == 'JAC' || teams[i] == 'CLE';
+	var teamFeildTypeTwoRight = teams[i] == 'SF' || teams[i] == 'OAK' || teams[i] == 'LA';
+	var teamFeildTypeTwoLeft = teams[i] == 'CIN' || teams[i] == 'TB' || teams[i] == 'TEN';
+	var teamFeildTypeThree = teams[i] == 'WAS';
+	if (teamFeildTypeOne) {
+		shell.exec('convert fields/' + teams[i] + '.png -crop 66x368+798+97 endzones/'
+			+ teams[i] + '.png');
+	}
+	else if (teamFeildTypeTwoRight) {
+		shell.exec('convert fields/' + teams[i] + '.png -crop 76x419+907+103 endzones/'
+			+ teams[i] + '.png');
+	}
+	else if (teamFeildTypeTwoLeft) {
+		shell.exec('convert fields/' + teams[i] + '.png -crop 76x419+41+103 endzones/'
+			+ teams[i] + '.png');
+	}
+	else if (teamFeildTypeThree) {
+		shell.exec('convert fields/' + teams[i] + '.png -crop 79x430+917+28 endzones/'
+			+ teams[i] + '.png');
+	}
+	else{
+		shell.exec('convert fields/' + teams[i] + '.png -crop 78x428+29+29 endzones/'
+			+ teams[i] + '.png');
+	}
+}
--- src/scripts/downloadImgScript.js
+++ src/scripts/downloadImgScript.js
@@ -8,7 +8,7 @@ var urls = {
 	'PIT': 'http://fields.gridiron-uniforms.com/graphics/Steelers2.png',
 	'PHI': 'http://fields.gridiron-uniforms.com/graphics/Eagles4.png',
 	'MIN': 'http://fields.gridiron-uniforms.com/graphics/Vikings2.png',
-	'MIA': 'http://fields.gridiron-uniforms.com/graphics/Dolphins4.png',
+	'MIA': 'http://fields.gridiron-uniforms.com/graphics/Dolphins.png',
 	'DAL': 'http://fields.gridiron-uniforms.com/graphics/Cowboys2.png',
 	'ARI': 'http://fields.gridiron-uniforms.com/graphics/Cardinals.png',
 	'ATL': 'http://fields.gridiron-uniforms.com/graphics/Falcons.png',
@@ -23,7 +23,18 @@ var urls = {
 	'NO': 'http://fields.gridiron-uniforms.com/graphics/Patriots.png',
 	'MIN': 'http://fields.gridiron-uniforms.com/graphics/Saints.png',
 	'NYG': 'http://fields.gridiron-uniforms.com/graphics/Giants.png',
-	'NYJ': 'http://fields.gridiron-uniforms.com/graphics/Jets.png'
+	'NYJ': 'http://fields.gridiron-uniforms.com/graphics/Jets.png',
+	'SF': 'http://fields.gridiron-uniforms.com/graphics/sb/XLVII.png',
+	'TB': 'http://fields.gridiron-uniforms.com/graphics/sb/XXXVII.png',
+	'TEN': 'http://fields.gridiron-uniforms.com/graphics/sb/XXXIV.png',
+	'DET': 'http://fields.gridiron-uniforms.com/images/full/DET_2001.png',
+	'HOU': 'http://img13.deviantart.net/8571/i/2015/054/5/9/football_field___texans__by_superman8193-d8jaudb.jpg',
+	'JAC': 'http://img15.deviantart.net/aa64/i/2015/054/8/c/football_field___jaguars__by_superman8193-d8jax57.jpg',
+	'OAK': 'http://fields.gridiron-uniforms.com/graphics/sb/XXXVII.png',
+	'LA': 'http://fields.gridiron-uniforms.com/graphics/sb/XXXIV.png',
+	'WAS': 'http://fields.gridiron-uniforms.com/graphics/sb/XXVI.png',
+	'CLE': 'http://img08.deviantart.net/0fa1/i/2015/054/b/1/football_field___browns__by_superman8193-d8jazfb.jpg',
+	'CIN': 'http://fields.gridiron-uniforms.com/graphics/sb/XLI.png'
 }
 
 var options = { 
